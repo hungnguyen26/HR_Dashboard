c79effc3f3825d547986d0d86f794524c0c3d32d
HỮU HƯNG <142713851+hungnguyen26@users.noreply.github.com>
2025-03-18 19:44:32 +0700

    Chia layout dashboard SB ADMIN 2

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const path = require("path");
 const { connectDB } = require("./config/database.js");
 const EventEmitter = require("events");
+const router = require("./routers/index.router.js");
+const expressLayouts = require('express-ejs-layouts');
 
 dotenv.config();
 
@@ -16,12 +18,14 @@ const startServer = async () => {
 
   app.use(express.static("public"));
   app.set("views", path.join(__dirname, "views"));
-  app.set("view engine", "pug");
+  app.set("view engine", "ejs");
+  app.use(expressLayouts);
+  app.set('layout', 'layouts/default');
 
   app.get("/", (req, res) => {
     res.send("Hello World!");
   });
-
+  router(app);
   app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   });
