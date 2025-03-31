68c325a480cf51aab62270104d38b12d6c838568
HỮU HƯNG <142713851+hungnguyen26@users.noreply.github.com>
2025-03-31 19:02:09 +0700

    Base 3.	Department & Job Title Management

--- routers/index.router.js
+++ routers/index.router.js
@@ -3,6 +3,8 @@ const employeeRouter = require("./employees.router");
 const payrollRouter = require("./payroll.router");
 
 module.exports = (app) => {
+  app.use("/", homeRouter);
+
   app.use("/home", homeRouter);
 
   app.use("/employees", employeeRouter);
